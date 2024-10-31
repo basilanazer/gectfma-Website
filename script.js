5ef10e18404e1438727c8189e254967bdeff3ed6
Kevin Andel Josy <kevinandeljosy11@gmail.com>
2024-10-31 13:08:13 +0530

    Changed font and well structured the code

--- script.js
+++ script.js
@@ -1,25 +1,28 @@
 function toggleMenu() {
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open")
-    icon.classList.toggle("open")
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
 }
-document.addEventListener('DOMContentLoaded', () => {
-    const title2 = document.querySelector('.title-2');
+document.addEventListener("DOMContentLoaded", () => {
+  const title2 = document.querySelector(".title-2");
 
-    const observer = new IntersectionObserver(entries => {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                entry.target.classList.add('animate');
-            } else {
-                entry.target.classList.remove('animate');
-            }
-        });
-    }, {
-        threshold: 0.5  // Adjust this value to determine when the animation triggers
-    });
-
-    if (title2) {
-        observer.observe(title2);
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("animate");
+        } else {
+          entry.target.classList.remove("animate");
+        }
+      });
+    },
+    {
+      threshold: 0.5, // Adjust this value to determine when the animation triggers
     }
+  );
+
+  if (title2) {
+    observer.observe(title2);
+  }
 });
